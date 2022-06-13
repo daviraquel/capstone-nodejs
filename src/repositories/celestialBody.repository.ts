767689846b1cb3975e64c1ccd18beee4f22f78db
Lucas Batista <batista.lucas3098@gmail.com>
2022-06-13 14:38:54 -0300

    feature/celestialBodies: create repository

---
+++ src/repositories/celestialBody.repository.ts
@@ -0,0 +1,30 @@
+import { hashSync } from "bcrypt";
+import { Repository } from "typeorm";
+import { AppDataSource } from "../data-source";
+import { CelestialBody } from "../entities/celestialBody.entity";
+import { ICelestialBodyRepo } from "../interfaces/celestialBody";
+
+class CelestialBodyRepository implements ICelestialBodyRepo {
+  private repo: Repository<CelestialBody>;
+
+  constructor() {
+    this.repo = AppDataSource.getRepository(CelestialBody);
+  }
+
+  save = async (celestialBody: CelestialBody) =>
+    await this.repo.save(celestialBody);
+
+  getAll = async () => await this.repo.find();
+
+  retrieve = async (payload: object) => {
+    return await this.repo.findOneBy({ ...payload });
+  };
+
+  update = async (id: string, payload: Partial<CelestialBody>) => {
+    return await this.repo.update(id, { ...payload });
+  };
+
+  delete = async (id: string) => await this.repo.delete(id);
+}
+
+export default new CelestialBodyRepository();
