f08ac6beaccdce08652e6111141047f011749857
MateusVSousa <min705555@gmail.com>
2022-06-14 22:49:18 -0300

    fix:controller

--- src/controllers/galaxy/galaxyCreate.controller.ts
+++ src/controllers/galaxy/galaxyCreate.controller.ts
@@ -2,7 +2,7 @@ import { Response, Request } from "express";
 import galaxyCreateService from "../../services/galaxy/galaxyCreate.service";
 
 const galaxyCreateController = async (req: Request, res: Response) => {
-  const { name, description } = req.params;
+  const { name, description } = req.body;
   const newGalaxy = await galaxyCreateService({ name, description });
 
   return res.status(newGalaxy.status).send(newGalaxy.create);
