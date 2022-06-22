393d120f3efe3010982c8f9f4329d13f14a13c0c
Lucas Batista <batista.lucas3098@gmail.com>
2022-06-22 10:19:11 -0300

    develop:change in data-source.ts

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,7 +5,7 @@ import path from "path";
 export const AppDataSource = new DataSource({
   type: "postgres",
   url: process.env.DATABASE_URL,
-  // ssl: { rejectUnauthorized: false },
+  ssl: { rejectUnauthorized: false },
   logging: false,
   entities: [path.join(__dirname, "./entities/**/*.{js,ts}")],
   migrations: [path.join(__dirname, "./migrations/**/*.{js,ts}")],
